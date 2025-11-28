684750993789dd921e7c32f1cd144a77d9db9f76
ENRICO SALVIOLI <enrico.salvioli.ext@cineca.it>
2025-11-28 16:53:21 +0100

    modifiche in  last

--- primo progetto/src/App.java
+++ primo progetto/src/App.java
@@ -1,6 +1,19 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("finalmentse");
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Test {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/dbFirstProject"; // nome del tuo database
+        String user = "root"; // o altro utente MySQL
+        String password = "laTuaPassword"; // metti la tua password
+
+        try {
+            Connection conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connessione riuscita!");
+            conn.close();
+        } catch (SQLException e) {
+            System.out.println("Errore di connessione: " + e.getMessage());
+        }
     }
 }
